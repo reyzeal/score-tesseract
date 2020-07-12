b61e1ba6d25270a685a3a830a22e111dece54c3d
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2020-07-12 23:29:57 +0700

    update

--- app.py
+++ app.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<form method="POST" enctype="multipart/form-data"><input type="file" name="file" accept="image/*"><button>Submit</button></form>'
+    with open('test','r') as f:
+        x = int(f.readline())
+    return f'Remaining Test:{x}<br><br><form method="POST" enctype="multipart/form-data"><input type="file" name="file" accept="image/*"><button>Submit</button></form>'
 
 @app.route('/', methods=['POST'])
 def proceed():
+    with open('test','r') as f:
+        x = int(f.readline())
+    if x <= 0:
+        return "Sorry, your test quota is zero"
+    else:
+        with open('test','w') as f:
+            f.write(str(x-1))
     if not os.path.isdir(os.path.join(os.path.dirname(__file__),'temp')):
         os.mkdir(os.path.join(os.path.dirname(__file__),'temp'))
     if 'file' not in request.files:
